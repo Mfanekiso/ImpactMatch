ed42bc4b44a68a571a4f3f3eb7fdd53bf3ce6032
DokotelaTech <nhlakaniphoxolo794@gmail.com>
2026-09-24 23:51:54 +0200

    api key hidden

--- Impact-Match/Backend/firebaseConfig.js
+++ Impact-Match/Backend/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxhzZ5WzDdyDTlG2wr-U0cQroxebRcixw",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "semester-2-project-c6235.firebaseapp.com",
   projectId: "semester-2-project-c6235",
   storageBucket: "semester-2-project-c6235.firebasestorage.app",
